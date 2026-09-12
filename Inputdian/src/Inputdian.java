b6bf6828ba7e9f29162faa053850f47617656ac8
Dian Marcell Hutagaol <2581032@unai.edu>
2026-09-12 15:58:45 +0700

    Menambah perhitungan subtotal, total belanja, dan kembalian

--- Inputdian/src/Inputdian.java
+++ Inputdian/src/Inputdian.java
@@ -31,6 +31,13 @@ public class Inputdian {
         System.out.print("Uang dibayar    : ");
         int uangDibayar = scanner.nextInt();
 
+        int subtotalBarang1 = hargaBarang1 * jumlahBarang1;
+        int subtotalBarang2 = hargaBarang2 * jumlahBarang2;
+        int subtotalBarang3 = hargaBarang3 * jumlahBarang3;
+
+        int totalBelanja = subtotalBarang1 + subtotalBarang2 + subtotalBarang3;
+        int kembalian = uangDibayar - totalBelanja;
+
         scanner.close();
     }
 }
